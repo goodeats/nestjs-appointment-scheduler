c3a6d837c9e8546bd7e947eb0757bf224b387e56
Pat Needham <patrick.h.needham@gmail.com>
2024-02-16 17:29:29 -0500

    doctor entity

---
+++ src/auth/doctor.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { DoctorState } from './doctor-state.enum';
+
+@Entity()
+export class Doctor {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  state: DoctorState;
+}
