7248466d9a27488de5470b78b8e3b5b142f92386
Pat Needham <patrick.h.needham@gmail.com>
2024-02-16 18:55:43 -0500

    comment about signup with other attributes besides auth creds

--- src/auth/dto/doctor-signup.dto.ts
+++ src/auth/dto/doctor-signup.dto.ts
@@ -1,30 +1,14 @@
-import {
-  IsEmail,
-  IsEnum,
-  IsString,
-  Matches,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsEnum, IsString, MaxLength, MinLength } from 'class-validator';
 import { DoctorState } from '../doctor-state.enum';
+import { AuthCredentialsDto } from './auth-credentials.dto';
 
-export class DoctorSignupDto {
-  @IsEmail()
-  email: string;
-
-  @IsString()
-  @MinLength(8)
-  @MaxLength(32)
-  // Password must contain at least
-  // - one lowercase letter
-  // - one uppercase letter
-  // - and one number
-  // Minimum length of 8 characters
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
-    message: 'password too weak',
-  })
-  password: string;
-
+// taking full doctor entity attributes on signup
+// didn't find an easy way to split the DTO at the controller
+// without the DoctorProfileDto also including the email and password
+// maybe that's a good thing for the body of the request to be all-inclusive and split later
+// may also be better UX/DX to have separate endpoints for signup and profile details
+// just trying things out as I explore NextJS
+export class DoctorSignupDto extends AuthCredentialsDto {
   @IsString()
   @MinLength(2)
   @MaxLength(32)
